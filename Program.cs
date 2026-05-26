c1963c8881681014a84450ee6c3f0265343752ff
lourdes colalillo <lourdescolalillo@gmail.com>
2026-05-26 09:55:40 -0300

    primer ejercicio

--- Program.cs
+++ Program.cs
@@ -1,8 +1,53 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+namespace PrimerEjercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+                int a;
+                int b;
+                a = 10;
+                b=a;
+                Console.WriteLine("valor de a:"+a);
+                Console.WriteLine("valor de b:"+b);
+            Console.WriteLine("-------------------------------------------");
 
-int a;
-int b;
-a = 10;
-b=a;
-Console.WriteLine("valor de a:"+a);
-Console.WriteLine("valor de b:"+b);
+            //EJERCICIO 1
+            //solicitar numero
+            Console.Write("Ingrese un numero entero para invertir: ");
+            string entrada = Console.ReadLine();
+            int numero;
+
+            //verificar si lo ingresado es un numero entero
+            if(int.TryParse(entrada, out numero))
+            {
+                if(numero > 0)
+                {
+                    int numeroInvertido = 0;
+                    int auxiliar = numero;
+
+                    //invertir el numero
+                    while(auxiliar > 0)
+                    {
+                        int ultimoDigito = auxiliar % 10;
+                        numeroInvertido = (numeroInvertido * 10) + ultimoDigito;
+                        auxiliar = auxiliar / 10;
+                    }
+                    //mostrar resultado por pantalla
+                    Console.WriteLine($"El numero invertido es: {numeroInvertido}");
+                }
+                else
+                {
+                    Console.WriteLine("El numero ingresado de ser mayor que cero");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error. Lo ingresado no es un numero entero");
+            }
+            Console.ReadKey();
+        }
+    }
+}
