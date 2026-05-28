d35cbafdff8f7a1b7389d9bfe38e46110510d6fe
lourdes colalillo <lourdescolalillo@gmail.com>
2026-05-28 17:09:10 -0300

    ejercicio 4

--- Program.cs
+++ Program.cs
@@ -115,4 +115,104 @@ do
     Console.Write("\n Desea hacer otro calculo? (S:Si/N:No): ");
     continuar = Console.ReadLine().ToUpper();
 } while (continuar == "S");
+
+/*EJERCICIO 4*/
+Console.Clear();
+/*pedir la cedena de texto*/
+Console.Write("Ingrese un texto: ");
+string cadenaPrincipal = Console.ReadLine();
+
+/*tener la longitud de la cadena */
+int longitud = cadenaPrincipal.Length;
+Console.WriteLine($"La longitud de la cadena es: {longitud}");
+
+/*pedir segunda cadena de texto*/
+Console.Write("Ingrese otro texto: ");
+string segundaCadena = Console.ReadLine();
+
+/*concatenar ambas cadenas*/
+string cadenaConcatenada = $"{cadenaPrincipal}{segundaCadena}";
+Console.WriteLine($"cadena concatenada: {cadenaConcatenada}");
+
+/*extraer una subcadena*/
+if (cadenaPrincipal.Length >= 7)
+{
+    string subCadena = cadenaPrincipal.Substring(2);
+    Console.WriteLine($"subcadena extraida: {subCadena}");
+}
+else
+{
+    Console.WriteLine("Error. No es posible obtener unasubcadena");
+}
+/*SEGUNDA PARTE DEL EJERCICIO*/
+Console.WriteLine("====MENU DE CALCULADORA====");
+Console.WriteLine("1.Sumar");
+Console.WriteLine("2.Restar");
+Console.WriteLine("3.Multiplicar");
+Console.WriteLine("4.Dividir");
+string opciones = Console.ReadLine();
+
+//validamos la opcion del usuario
+if (opciones == "1" || opciones == "2" || opciones == "3" || opciones == "4")
+{
+    //pedimos y validamos el primer numero
+    Console.Write("Ingrese el primer numero: ");
+    if (double.TryParse(Console.ReadLine(), out double num1))
+    {
+        Console.Write("Ingrese el segundo numero: ");
+        if (double.TryParse(Console.ReadLine(), out double num2))
+        {
+            double resultados = 0;
+
+            switch (opciones)
+            {
+                case "1":
+                    resultados = num1 + num2;
+                    Console.WriteLine($"La suma entre {num1.ToString()} y {num2.ToString()} es igual a {resultados.ToString()}");
+                    break;
+
+                case "2":
+                    resultados = num1 - num2;
+                    Console.WriteLine($"La resta entre {num1.ToString()} y {num2.ToString()} es igual a {resultados.ToString()}");
+                    break;
+
+                case "3":
+                    resultados = num1 * num2;
+                    Console.WriteLine($"La multiplicacion entre {num1.ToString()} y {num2.ToString()} es igual a {resultados.ToString()}");
+                    break;
+
+                case "4":
+                    if (num2 != 0)
+                    {
+                        resultados = num1 / num2;
+                        Console.WriteLine($"La division entre {num1.ToString()} y {num2.ToString()} es igual a {resultados.ToString()}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error.No se puede realizar la division en cero");
+                    }
+                    break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Error.Lo ingresado no es un numero");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Error. Lo ingresado no es un numero");
+    }
+}
+else
+{
+    Console.WriteLine("No es una opcion valida de menu");
+}
+
+/*recorrer cadena*/
+foreach (char letra in cadenaPrincipal)
+{
+    Console.Write($"[{letra}] ");
+}
+Console.WriteLine();
 Console.ReadKey();
